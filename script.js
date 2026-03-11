a84d65f90666df120da7b22baa3b8d943fbafcd1
Katya <puhhova@hotmail.com>
2026-03-11 16:58:22 +0000

    Formatting basic logic for password requirements

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const passwordInput = document.getElementById("password");
+const usernameInput = document.getElementById("username");
 
+const strengthText = document.getElementById("strengthText");
+const meterfill = document.getElementById("meterfill");
+const feedback = document.getElementById("feedback");
+
+const lengthReq = document.getElementById("lengthReq");
+const caseReq = document.getElementById("caseReq");
+const numberReq = document.getElementById("numberReq");
+const symbolReq = document.getElementById("symbolReq");
