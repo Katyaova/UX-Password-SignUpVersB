d33bcbec83c3a7ae753e83e38f5161373e5a0a29
Katya <puhhova@hotmail.com>
2026-03-11 17:10:31 +0000

    password Strength Checker first level

--- script.js
+++ script.js
@@ -15,4 +15,22 @@ const warningModal = document.getElementById("continueBtn");
 const improveBtn = document.getElementById("improveBtn");
 const useAnywayBtn = document.getElementById("useAnywayBtn");
 
-fun
+function checkPasswordStrength(password, username) {
+    let score = 0;
+    let warning = [];
+
+    const hasLength = password.length >=8;
+    const hasCase = /[A-Z]/.test(password) && /[a-z]/.test(password);
+    const hasNumber = /[0-9]/.test(password);
+    const hasSymbol = /[^A-Za-z0-9]/.test(password);
+
+    lengthReq.textContent = `${hasLength ? "✔" : "✖"} At least 8 characters`;
+    hasCase.textContent = `${hasCase ? "✔" : "✖"} Includes uppercase & lowercase`;
+    hasNumber.textContent = `${hasNumber ? "✔" : "✖"} Includes a number`;
+    hasSymbol.textContent = `${hasSymbol ? "✔" : "✖"} Includes a symbol`;
+
+    if (hasLength) score++;
+    if (hasCase) score++;
+    if (hasNumber) score++;
+    if (hasSymbol) score++;
+}
